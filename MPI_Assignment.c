1052fc01b37204df6dfb669d47a30bbe5e6d56e3
chubenko <chubenko@gwu.edu>
2015-11-23 00:24:49 -0500

    add data.txt file to the repo

--- MPI_Assignment.c
+++ MPI_Assignment.c
@@ -1,64 +1,94 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+double get_wall_time();
 
 int main(int argc, char * argv[])
 {
-	int i, j, n;
-	int N = 5; //array size
+	int i, j, n, size;
+	int N = 8; //size of the matrix
 	int Nc = 10; //number of colors
-	int total_image[N][N], scattered_image[N], local_hist[Nc], total_hist[Nc];
-	
+	int total_image[N*N];
+	int local_hist[Nc], total_hist[Nc];
+	double time1, time2, time3, time4, time5, time6;
+
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&n);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	
+        int scat_size = N*N/size;
+	int scattered_image[scat_size];
 
 	for(i = 0; i < Nc; i++){
 		local_hist[i] = 0;
-	//	total_hist[i] = -1;
 	}
 
 	if(n == 0){
+		printf("\nSize:%i\n",size);
 		for(i = 0; i < N; i++){
 			for(j = 0; j < N; j++){
-				total_image[i][j] = rand()%Nc;
-			}
-		}
-		for(i = 0; i < N; i++){
-			for(j = 0; j < N; j++){
- 	   			printf("%i \t",total_image[i][j]);
+				total_image[i*N+j] = rand()%Nc;
+				printf("%i \t",total_image[i*N+j]);
 			}
 			printf("\n");
 		}
 	}
 
- 
-	MPI_Scatter(&total_image,N,MPI_INT,&scattered_image,N,MPI_INT,0,MPI_COMM_WORLD);
-        printf("\n Rank: %i \n ", n);
-	for(i = 0; i < N; i++){
+	printf("\n Rank: %i ", n);
+	printf("\n------------------------------------\n");
+
+ 	time1 = get_wall_time();
+	MPI_Scatter(&total_image,scat_size,MPI_INT,&scattered_image, scat_size,MPI_INT,0,MPI_COMM_WORLD);
+        time2 = get_wall_time();
+	
+	for(i = 0; i < scat_size; i++){
 		printf("%i \t",scattered_image[i]);
 	}
 
-	for(i = 0; i < N; i++){
+	time3 = get_wall_time();
+	for(i = 0; i < scat_size; i++){
 		for(j = 0; j < Nc; j++){
 			if (scattered_image[i] == j)
 				local_hist[j] += 1;
 		}
 	}
-        
-        printf("\n Rank: %i \n", n);
-	for(i = 0; i < Nc; i++) 
+//	printf("\n Rank:%i Time to scatter pixels to processes = \t%lf\n",n, time2-time1);
+	time4 = get_wall_time();        
+	printf("\n Time to calculate local histogram on process %i  = \t%lf\n",n,time4-time3);
+/*
+         printf("\n Rank: %i \n", n);
+         for(i = 0; i < Nc; i++) 
 		printf("%i ",local_hist[i]);
 		printf("\n");
+*/
+        MPI_Barrier(MPI_COMM_WORLD);
+	time5 = get_wall_time();
+	MPI_Reduce(&local_hist,&total_hist,Nc,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+	time6 = get_wall_time();       
+//	printf("\n Rank:%i Time to reduce local histograms into a global histogram = \t%lf\n",n,time6-time5);
 
         MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Reduce(&local_hist,&total_hist,Nc,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-       
-        MPI_Barrier(MPI_COMM_WORLD);
 	if(n == 0) {
-		printf("Total: \n");
+		printf("\nTotal histogram:");
+		printf("\n-----------------------------------\n");
 		for(i = 0; i < Nc; i++)	printf(" %i ",total_hist[i]); 
 		printf("\n");
 	}
 	
 	MPI_Finalize();
 }
+
+
+double get_wall_time(){
+	struct timeval time;
+	gettimeofday(&time,NULL);
+	return (double)time.tv_sec+(double)time.tv_usec/1000000; 
+}
+
+
+
+
+
+
